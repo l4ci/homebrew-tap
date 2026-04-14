b3486dccac56f8405e47b0f148657e060a815ccb
Volker Otto <hello@volkerotto.net>
2026-04-14 13:09:10 +0200

    Update MocoCompanion to v0.5.0

--- Casks/mococompanion.rb
+++ Casks/mococompanion.rb
@@ -1,6 +1,6 @@
 cask "mococompanion" do
-  version "0.4.1"
-  sha256 "20d8e9b68407b5170643d8d25911c332a3e701a7766f70a65aac8ca24888a4bc"
+  version "0.5.0"
+  sha256 "c1cecd0283133963449a71dcaae0c8824a99e80cd6caa751355159152bba2c79"
 
   url "https://github.com/l4ci/MocoCompanion/releases/download/v#{version}/MocoCompanion-#{version}.zip"
   name "MocoCompanion"
